3a2093c06f0c907fe06c11ee6b9c99ba42e67a89
Round Squisheen <zhangjingming131@gmail.com>
2025-10-17 17:08:23 +0800

    增加15分钟内的请求次数

--- src/app.js
+++ src/app.js
@@ -18,8 +18,8 @@ const PORT = process.env.PORT || 3001;
 app.use(helmet());
 app.use(cors());
 app.use(rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+  windowMs: 15 * 60 * 1000,  // 15分钟
+  max: 300,                  // 调整为300次
   message: 'Too many requests, please try again later'
 }));
 app.use(express.json({ limit: '10mb' }));
